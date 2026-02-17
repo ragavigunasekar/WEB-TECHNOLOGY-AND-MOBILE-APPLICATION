94349f65641793e6139b595fb16ec64baff84124
RAGAVI G <ragavi.g.2024.aiml@rajalakshmi.edu.in>
2026-02-18 00:37:26 +0530

    Add files via upload

---
+++ HelloWorldServlet/HelloServlet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head><title>Hello Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
